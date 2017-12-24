f3fd32a4575b1aa08591cfceef96ea3fa5f80c7e
semitro <semitro_8@mail.ru>
2017-12-24 23:21:06 +0300

    Свёрстана стартовая страница

--- src/main/java/vt/smt/controllers/HelloPageController.java
+++ src/main/java/vt/smt/controllers/HelloPageController.java
@@ -1,9 +1,42 @@
 package vt.smt.controllers;
 
 import org.ocpsoft.rewrite.annotation.Join;
+import org.ocpsoft.rewrite.el.ELBeanName;
 import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import javax.faces.bean.ManagedProperty;
 
 @Scope(value = "session")
 @Join(path = "/", to = "/index.jsf")
+@Component(value = "helloPageController")
+@ELBeanName(value = "helloPageController")
 public class HelloPageController {
+
+    @ManagedProperty("username")
+    private String username;
+
+    @ManagedProperty("password")
+    private String password;
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    // It invoked when user presses Войти button
+    public void signIn(){
+        System.out.println(username + "-" + password);
+    }
 }
--- src/main/java/vt/smt/security/AppSecurityConfig.java
+++ src/main/java/vt/smt/security/AppSecurityConfig.java
@@ -21,6 +21,7 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().anyRequest().permitAll();
+
 //        http.authorizeRequests()
 //                .antMatchers("/protected/**").access("hasRole('ROLE_ADMIN')")
 //                .antMatchers("/confidential/**").access("hasRole('ROLE_SUPERADMIN')")
