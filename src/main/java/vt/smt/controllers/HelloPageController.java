a0accc9e5611456fe416c41c774269c646cf3612
semitro <semitro_8@mail.ru>
2018-03-14 21:10:51 +0300

    Начало страницы персонажа. Персонаж залогиненного юзера передаётся на другую страницу. Разборка с аннотациями и тем, кто именно делает DI (видимо, всё-таки Spring)

--- src/main/java/vt/smt/controllers/HelloPageController.java
+++ src/main/java/vt/smt/controllers/HelloPageController.java
@@ -3,36 +3,33 @@ package vt.smt.controllers;
 import org.ocpsoft.rewrite.annotation.Join;
 import org.ocpsoft.rewrite.el.ELBeanName;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
-import vt.smt.db.repositories.CharacterRepository;
 import vt.smt.db.repositories.UsersRepository;
 import vt.smt.ent.net.Users;
 
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ManagedProperty;
-import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 
-@ManagedBean
-@SessionScoped
+//@ManagedBean
+//@SessionScoped
 @Join(path = "/", to = "/index.jsf")
-@Component(value = "helloPageController")
+@Component(value = "helloPageController" )
+@Scope(value = "session")
 @ELBeanName(value = "helloPageController")
 
 public class HelloPageController {
 
-    @ManagedProperty("username")
+    //managedProperty - не нужно! Оно используется для DI с помощью JSF
     private String username;
 
-    @ManagedProperty("password")
     private String password;
 
     @Autowired
     private UsersRepository usersRepository;
 
-    @Autowired
-    private CharacterRepository characterRepository;
-
+    public HelloPageController(){
+        System.err.println("hello pageContorller constructor");
+    }
     public String getUsername() {
         return username;
     }
@@ -49,19 +46,16 @@ public class HelloPageController {
         this.password = password;
     }
 
-    // It invoked when user presses Войти button
+    // It's invoked when user presses Войти button
     public String signIn(){
-        System.out.println(username + "-" + password);
         FacesContext context = FacesContext.getCurrentInstance();
 
         Users user = usersRepository.findByLogin(username);
-        System.out.println(user.getUserId() + " -id");
         if(user == null){
             System.err.println("Пользователь " + username + " не найден в базе");
         }
         else {
             try {
-                System.out.print(new String());
                 context.getExternalContext().getSessionMap().put("GameCharacter", user.getGameCharacters().get(0));
             }
             catch (RuntimeException e){
