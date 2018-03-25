a21788847190e4cb2e4a4215c9cda47a8e69b29e
trinistr <trinistr@gmail.com>
2018-03-26 00:29:46 +0300

    Cleaned up SQL scripts

--- dbRoutine/scripts/setUpResources.sql
+++ dbRoutine/scripts/setUpResources.sql
@@ -1,2 +1,8 @@
 insert into resource (name, type, url) values
 	('default character', 'img', '/img/defaultCharacter.png');
+
+insert into resource(name, type, url) values
+	('bloodGroup', 'img','/img/skills/bloodGroup.png'),
+	('gainOverload', 'img','/img/skills/gainOverload.png'),
+	('extrimVocal', 'img','/img/skills/extrimVocal.png'),
+	('regularAttack', 'img', '/img/skills/regularAttack.png');
