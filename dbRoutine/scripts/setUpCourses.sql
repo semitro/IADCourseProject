35612c7c00022827775f30a8b021a4625f052cf0
semitro <semitro_8@mail.ru>
2018-03-25 22:19:07 +0300

    Тесты в бд

--- dbRoutine/scripts/setUpCourses.sql
+++ dbRoutine/scripts/setUpCourses.sql
@@ -9,3 +9,16 @@ insert into article(title, subtitle, content, course_id) values
 (select course_id from course where title = 'Аккорды') ),
 ('Септаккорды', 'четырёхэтажные аккоды', 'содержание статьи о септаккордах', 
 	(select course_id from course where title = 'Аккорды') );
+
+-- В скрипт награды просто кладём название абилки, которую мы получим
+insert into script(name, script) values ('getGroupOfBloodSkill', 'Узнать группу крови на рукаве');
+
+insert into test(title, description, reward_script_id) values 
+('Тест об интервалах', 'Насколько хорошо вы знаете музыкальные интервалы?', 
+	(select script_id from script where name = 'getGroupOfBloodSkill'));
+
+insert into question(content, answer, wrong1, wrong2, wrong3, test_id) values
+('Что такое интервал?', 'Две нотки, связанные гармонически или мелодически',
+	'Это заведомо ошибочный  ответ', 'Стеклянные окуни', 'Ну, это типа аккорд, но не совсем',
+	(select test_id from test where title ='Тест об интервалах'));
+
