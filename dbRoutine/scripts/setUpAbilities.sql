614737d3d83c6e0ac667ea9ea53b70a941b46408
trinistr <trinistr@gmail.com>
2018-03-26 07:01:17 +0300

    Updated scripts

--- dbRoutine/scripts/setUpAbilities.sql
+++ dbRoutine/scripts/setUpAbilities.sql
@@ -2,15 +2,15 @@ insert into script (name, script) values ('emptyScript', ''), ('sayHello', 'me.s
 ('regularAttack', 'enemy.regularAttack()'),
 ('extremeVocal', 'enemy.extremeVocal()'),('groupOfBlood', 'me.groupOfBlood()'),('gainOverLoad', 'me.gainOverload()');
 
-insert into ability(name, passive, description, min_exp_to_use, image_resource_id, ability_script_id)
+insert into ability(name, passive, description, min_exp_to_use, image_resource_id, ability_script_id, cooldown)
 values
 ('Экстрим вокал', false, 'Пение скримом наносит фиксированный чистый урон по противнику!', 12000, (select resource_id from resource where name = 'extrimVocal'),
-	(select script_id from script where name = 'extremeVocal')),
+	(select script_id from script where name = 'extremeVocal'), 1),
 ('Узнать группу крови на рукаве', false, 'Знание своей группы крови увеличивает здоровье', 10, (select resource_id from resource where name = 'bloodGroup'),
-	(select script_id from script where name = 'groupOfBlood')),
+	(select script_id from script where name = 'groupOfBlood'), 1),
 ('Выкрутить Gain', false, 'Атака увеличивается в два раза на три хода. Повторное использование обновляет счётчик'
 	, 200, (select resource_id from resource where name = 'gainOverload'),
-	(select script_id from script where name = 'gainOverLoad')),
+	(select script_id from script where name = 'gainOverLoad'), 1),
 ('Обычная атака', false, 'Урон зависит от атаки, защиты соперника и ритма', 0, (select resource_id from resource where name = 'regularAttack'),
-	(select script_id from script where name = 'regularAttack'));
+	(select script_id from script where name = 'regularAttack'), 0);
 
